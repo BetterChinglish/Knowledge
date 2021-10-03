a7398d8e9f4845fe3d933c2edd794a0156f0df28
BetterChinglish <2199840669@qq.com>
2021-10-03 11:00:47 +0800

    10.3

--- 06_vue/Vue3_ITFengBoVideo/Vue-cli/demo5-vuex/src/store/index.js
+++ 06_vue/Vue3_ITFengBoVideo/Vue-cli/demo5-vuex/src/store/index.js
@@ -5,6 +5,9 @@ export default createStore({
     num:0,
     dnum:0
   },
+
+
+  // mutation必须是同步函数
   mutations: {
 
     // functionName(state, payload){}
@@ -17,8 +20,8 @@ export default createStore({
     // })
 
     // 例
-    addN(state, scores){
-      state.dnum+=scores.math
+    addN(state, mathScores){
+      state.dnum=state.dnum + mathScores;
     },
 
     // 另一种提交方式
@@ -35,8 +38,16 @@ export default createStore({
       state.dnum--;
     }
 
-    
 
+    // 使用常量代替mutation事件类型
+    // mutation-type.js:
+    // export const mutationFunction = 'mutationFunctionName'    
+    // 此文件引入mutation-type.js中的mutationFunction
+    // [mutationFunction](state){}
+    // 也就是把名字全部提出来单独形成一个文件, 便于多人协作或者其他插件对文件的扫描
+
+
+    // 提交mutation: 看About.vue
     
   },
   getters:{
@@ -62,12 +73,32 @@ export default createStore({
   },
   actions: {
 
+    // action类似于mutation
+    // 但是
+    // action是提交mutation, 而不是直接变更状态
+    // action可以使用异步操作
+
+    // 结构如下:
+    // functionName(context){}
+    // context是与store具有相同方法和属性的context对象
+    // 例如可以使用context.commit()
+
+    // 但是如上使用太麻烦, 因为每次使用都要使用context对象来访问其中的方法与属性
+    // 可以使用解构赋值, 如下:
+    // functionName({commit, state....}){
+    //  // commit('...')
+    //  // state.property...
+    // }
+
+    // 异步操作
     demo({commit,state}){
       setTimeout(()=>{
         state.num=100;
       },3000)
     }
 
+    // 分发action使用store.dispatch('functionName', payload), 支持载荷发送或者对象发送
+    // 也可以在methods中使用mapActions进行映射, 和之前的类似
 
   },
   modules: {
--- 06_vue/Vue3_ITFengBoVideo/Vue-cli/demo5-vuex/src/views/About.vue
+++ 06_vue/Vue3_ITFengBoVideo/Vue-cli/demo5-vuex/src/views/About.vue
@@ -8,18 +8,52 @@
     <button @click="useDemo">click me</button>
     <p>actions to change num</p>
     {{$store.state.num}}
+
+    <br>
+    <p>
+      dnum:{{$store.state.dnum}}
+    </p>
+    <br>
+    <input v-model="mathScore"><button @click="addN(Number(mathScore))">add this to dnum</button>
+
+    <p>
+      {{mathScore}}
+    </p>
   </div>
 </template>
 
 
 <script>
-import {mapState, mapGetters} from 'vuex'
+import {mapState, mapGetters, mapMutations} from 'vuex'
 export default {
   name:'About',
+  data() {
+    return {
+      mathScore:0
+
+    }
+  },
   methods: {
     useDemo(){
       this.$store.dispatch('demo');
-    }
+    },
+   
+
+    // 提交mutation
+    // 可以使用传统的commit, index.js已经提到两种风格
+    // 还可以使用mapMutations
+    // 在methods中使用mapMutation
+    // ...mapMutations({
+    //   add:'addN'  // 将this.addN(scores) 映射为 this.$store.commit('addN',scores);
+    // })
+
+    ...mapMutations([
+      'addN'
+    ])
+
+
+
+
   },
 
   // computed, 为每个state声明一个computed很麻烦, 可以使用mapState
@@ -47,26 +81,26 @@ export default {
 
 
     computed:{
-      localComputed:{
-        // ...
-      },
-      // ...
+      // localComputed:{
+      //   // ...
+      // },
+      // // ...
 
 
-      // 使用三点运算符将其解构赋值
-      ...mapState({
-        // ...
-      }),
+      // // 使用三点运算符将其解构赋值
+      // ...mapState({
+      //   // ...
+      // }),
 
       // 使用mapGetters
       // mapGetters作用仅仅是将getter映射到局部计算属性
-      ...mapGetters([
+      // ...mapGetters([
 
         // 'getterName',// 注意这里是es6语法
         // 如果另取名字, 使用对象赋值写法 aliasName:'getterName'
+      // ])
 
-      ])
-
+      
 
 
     }
