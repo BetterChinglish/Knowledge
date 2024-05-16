03ca8b96bebcb75c0b682d5542a6c30785bc7e37
七色堇 <59131359+BetterChinglish@users.noreply.github.com>
2024-05-16 17:50:57 +0800

    Update webpack.config.js
    review

--- 17 webpack/04 打包css资源/webpack.config.js
+++ 17 webpack/04 打包css资源/webpack.config.js
@@ -16,14 +16,17 @@ module.exports={
         rules:[
             {
                 // 正则规则
-                // \是转义符, \.即为字符串.
+                // \是转义符, \.即为字符串 '.'
                 // $表示匹配以前面字符串结尾
                 // 这里即为对以.css结尾的文件使用css-loader和style-loader
                 test: /\.css$/,
 
                 // 这里从右往左依次使用loader
                 // 即先使用css-loader再使用style-loader
-                use:['style-loader', 'css-loader']
+                use:[
+                    'style-loader',    // 将js中的css通过创建style标签添加到html文件中生效
+                    'css-loader'    // 将css文件编译成commonjs的模块到js中
+                ]
                 // 如果只需要使用一个loader则use属性改为
                 // loader: 'css-loader'
             }
