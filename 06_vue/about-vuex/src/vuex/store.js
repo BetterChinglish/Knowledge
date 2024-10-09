c59b532026693563c4ee9639c0b27396cc44d04e
zhangjunjie <zhangjunjie73@huawei.com>
2024-10-09 17:17:46 +0800

    getters、mutations、commit实现

--- 06_vue/about-vuex/src/vuex/store.js
+++ 06_vue/about-vuex/src/vuex/store.js
@@ -14,17 +14,35 @@ const install = (_Vue) => {
 // Vuex的东西如何进行的初始化以及提供的功能
 class Store{
   constructor(options) {
-    // console.log(options);
     let state = options.state;
+    
+    this.getters = {};
+    
+    // 构建计算属性配置对象
+    const computed = {};
+    
+    // getValue即为store定义的getter方法
+    objForEach(options.getters, (key, getValue) => {
+      computed[key] = () => {
+        return getValue(state)
+      }
+      // 后续获取store.getters的时候其实就是访问_vm的computed, 这样就实现了getters与computed一样的效果n
+      Object.defineProperty(this.getters, key, {
+        get: () => this._vm[key]
+      })
+    })
+    
     this._vm = new Vue({
       data: {
         // 以$开头的变量不会代理到vue的实例上,而是在_data上
         $$state: options.state
       }
-    });   
-    // console.log(this._vm);
-    
-    
+    });
+  }
+  
+  // 用户调用commit时传入需要调用mutations对应的方法，type确定是哪个方法，payload是传入的参数
+  commit = (type, payload) => {
+    this._mutations[type](payload)
   }
   
   get state() {
