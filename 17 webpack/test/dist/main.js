686ffac999309b0ec2e7ecda4cc320d0dac622fc
BetterChinglish <2199840669@qq.com>
2025-03-20 22:14:32 +0800

    webpack基础使用

---
+++ 17 webpack/test/dist/main.js
@@ -0,0 +1,15 @@
+(() => {
+  var __webpack_modules__ = ({
+    "./src/index.js":
+      (() => {
+        
+        eval( "console.log('hello webpack')\n\n//# sourceURL=webpack://test/./src/index.js?" );
+        
+      })
+    
+  });
+  var __webpack_exports__ = {};
+  __webpack_modules__["./src/index.js"]();
+  
+})()
+;
---
+++ 17 webpack/test/webpack.config.js
@@ -0,0 +1,11 @@
+
+const { resolve } = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: "bundle.js"
+  }
+}
